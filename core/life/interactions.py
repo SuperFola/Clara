8871d838eee98ee0c4348fe7daacd65ab5c9b081
Loodoor <folaefolc@outlook.fr>
2016-02-05 22:46:46 +0100

    added a class for the interaction with an / some objects

--- core/life/interactions.py
+++ core/life/interactions.py
@@ -16,7 +16,13 @@ class HumanInteraction(Interaction):
     def __init__(self, name: str, priority: float, with_persons: tuple, *interests_involved):
         super().__init__(name, priority, *interests_involved)
         self.with_persons = with_persons
-    
+
     def get_participants(self) -> tuple:
         """Return the list of participants in this interaction"""
         return self.with_persons
+
+
+class HumanObjectInteraction(Interaction):
+    def __init__(self, name: str, priority: float, with_objects: tuple, *interests_involved):
+        super().__init__(name, priority, *interests_involved)
+        self.with_objects = with_objects
